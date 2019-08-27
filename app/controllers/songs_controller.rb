826cad9f0346aa7aa86e97f83c9955696a0bc03a
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-08-27 16:25:36 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,28 @@
+class SongsController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/songs' do
+    @songs = Song.all
+    erb :'songs/index'
+  end
+
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'songs/new'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
+
+  post '/songs/:slug' do
+    @artist = Artist.find_or_create_by(name: params[:song][:artist])
+    @song = Song.create(name: params[:song][:name], artist: @artist)
+
+    erb :'songs/show'
+  end
+
+end
